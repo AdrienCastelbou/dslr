9ab4b8dea25962b53c58706ad68362d4f65c1d2d
Adrien Castelbou <acastelbou1@gmail.com>
2022-12-12 15:09:08 +0100

    part I ok, maybe percentiles adjustements

---
+++ TinyStatistician.py
@@ -0,0 +1,92 @@
+from math import sqrt
+import numpy as np
+from typing import Union
+
+class TinyStatistician:
+
+    @staticmethod
+    def count(x):
+        try:
+            l = len(x)
+            res = 0.0
+            for elem in x:
+                if not isinstance(elem, (float, int)):
+                    return None
+                res += elem
+            return res
+        except:
+            return None
+
+    @staticmethod
+    def mean(x: Union[list, np.array]) -> float:
+        try:
+            l = len(x)
+            res = 0.0
+            for elem in x:
+                if not np.isreal(elem):
+                    print(elem, type(elem))
+                    return None
+                res += elem
+            return res / l
+        except:
+            return None
+
+    @staticmethod
+    def median(x: Union[list, np.array]) -> float:
+        try:
+            sorted_x = sorted(x)
+            l = len(x)
+            if l == 0 or not all(np.isreal(elem) for elem in x):
+                return None
+            if l % 2:
+                return sorted_x[int(len(x) / 2)]
+            else:
+                return (sorted_x[int(len(x) / 2) - 1] + sorted_x[int(len(x) / 2)]) / 2
+        except:
+            return None
+
+    @staticmethod
+    def quartile(x: Union[list, np.array]):
+        try:
+            l = len(x)
+            if l == 0 or not all(np.isreal(elem) for elem in x):
+                return None
+            sorted_x = sorted(x)
+            print(x)
+            return [float(sorted_x[int(l / 4)]), float(sorted_x[int(l * 3 / 4)])]
+        except:
+            return None
+
+    @staticmethod
+    def percentile(x: Union[list, np.array], p: Union[int, float]) -> float:
+        try:
+            l = len(x)
+            sorted_x = sorted(x)
+            return sorted_x[int(np.ceil(p / 100 * l)) - 1]
+        except:
+            return None
+
+    @staticmethod
+    def var(x: Union[list, np.array]) -> float:
+        try:
+            l = len(x)
+            if l == 0 or not all(np.isreal(elem) for elem in x):
+                return None
+            mean = TinyStatistician.mean(x)
+            res = 0
+            for elem in x:
+                res += (elem - mean) * (elem - mean)
+            return res / (l  - 1)
+        except:
+            return None
+
+
+    @staticmethod
+    def std(x: Union[list, np.array]) -> float:
+        try:
+            l = len(x)
+            if l == 0 or not all(np.isreal(elem) for elem in x):
+                return None
+            return sqrt(TinyStatistician.var(x))
+        except:
+            return None
---
+++ describe.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+import sys
+from TinyStatistician import TinyStatistician as TS
+
+def load_dataset():
+    if len(sys.argv) != 2:
+        raise Exception("Error : Wrong number of arguments -> Usage : python3 describe.py path/to/dataset.csv")
+    df = pd.read_csv(sys.argv[1])
+    return df
+
+def describe_feature(datas):
+    datas = datas[~np.isnan(datas)]
+    infos = []
+    infos.append(len(datas))
+    infos.append(TS.mean(datas))
+    infos.append(TS.std(datas))
+    infos.append(np.min(datas))
+    infos.append(TS.percentile(datas, 25))
+    infos.append(TS.percentile(datas, 50))
+    infos.append(TS.percentile(datas, 75))
+    infos.append(np.max(datas))
+    return infos
+
+def describe_df(df):
+    print(df.describe())
+    described = pd.DataFrame(index=pd.Index(["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"]))
+    for feature in df:
+        if not is_numeric_dtype(df[feature]) or df[feature].isnull().values.all():
+            continue
+        datas = df[feature].to_numpy()
+        described[feature] = describe_feature(datas)
+    print(described)
+
+def main():
+    try:
+        df = load_dataset()
+    except Exception as e:
+        print(e, file=sys.stderr)
+        return
+    describe_df(df)
+    
+    
+
+if __name__ == "__main__":
+    main()
