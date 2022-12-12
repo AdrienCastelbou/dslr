d5463c0615171e8bf47f66cd22e1cbca8866d4e2
Adrien Castelbou <acastelbou1@gmail.com>
2022-12-12 19:19:01 +0100

    display only one scat

--- scatter_plot.py
+++ scatter_plot.py
@@ -15,19 +15,14 @@ def show_scatter_plot(df):
     Slytherin_df = df[df["Hogwarts House"] == "Slytherin"]
     Gryffindor_df = df[df["Hogwarts House"] == "Gryffindor"]
     Hufflepuff_df = df[df["Hogwarts House"] == "Hufflepuff"]
-    #df.drop('Hogwarts House', inplace=True, axis=1)
-    df = df[classes]
-    for x_feature in df:
-        for y_feature in df:
-            if x_feature != y_feature:
-                plt.scatter(Ravenclaw_df[x_feature].to_numpy(), Ravenclaw_df[y_feature].to_numpy(), label="Ravenclaw", alpha=0.5)
-                plt.scatter(Slytherin_df[x_feature].to_numpy(), Slytherin_df[y_feature].to_numpy(), label="Slytherin", alpha=0.5)
-                plt.scatter(Gryffindor_df[x_feature].to_numpy(), Gryffindor_df[y_feature].to_numpy(), label="Gryffindor", alpha=0.5)
-                plt.scatter(Hufflepuff_df[x_feature].to_numpy(), Hufflepuff_df[y_feature].to_numpy(), label="Hufflepuff", alpha=0.5)
-                plt.xlabel(x_feature)
-                plt.ylabel(y_feature)
-                plt.legend()
-                plt.show()
+    plt.scatter(Ravenclaw_df["Astronomy"].to_numpy(), Ravenclaw_df["Defense Against the Dark Arts"].to_numpy(), label="Ravenclaw", alpha=0.5)
+    plt.scatter(Slytherin_df["Astronomy"].to_numpy(), Slytherin_df["Defense Against the Dark Arts"].to_numpy(), label="Slytherin", alpha=0.5)
+    plt.scatter(Gryffindor_df["Astronomy"].to_numpy(), Gryffindor_df["Defense Against the Dark Arts"].to_numpy(), label="Gryffindor", alpha=0.5)
+    plt.scatter(Hufflepuff_df["Astronomy"].to_numpy(), Hufflepuff_df["Defense Against the Dark Arts"].to_numpy(), label="Hufflepuff", alpha=0.5)
+    plt.xlabel("Astronomy")
+    plt.ylabel("Defense Against the Dark Arts")
+    plt.legend()
+    plt.show()
 
 def main():
     try:
