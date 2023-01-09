ce4435a7a941c5aa2290502f6167cde40bd496c9
Adrien Castelbou <acastelbou1@gmail.com>
2023-01-09 11:23:36 +0100

    reduce used stuff

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-COURSES = ["Astronomy", "Herbology", "Divination", "Muggle Studies", "Ancient Runes", "History of Magic", "Transfiguration", "Potions", "Care of Magical Creatures", "Charms", "Flying"]
+COURSES = ["Astronomy", "Herbology", "Divination", "Muggle Studies", "Ancient Runes", "History of Magic", "Transfiguration", "Charms", "Flying"]
 RAVENCLAW = 0
 SLYTHERIN = 1
 GRYFFINDOR = 2
